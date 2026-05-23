835052e2bd08cdc4af3fff0030d91c44e72a067e
Sonya <sdanilova025@gmai.com>
2026-05-24 00:10:50 +0700

    добавила флаги --info и --help

--- app/main.go
+++ app/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"notes-app/config"
 	"notes-app/notes"
+	"os"
 )
 
 func main() {
+	helpFlag := flag.Bool("info", false, "Показать подробное пояснение к запуску проекта и архитектуре")
+	flag.Usage = func() {
+		fmt.Println("<МЕНЕДЖЕР ЗАМЕТОК>\nинструкция по использованию")
+		fmt.Println("\nДоступные флаги:")
+		flag.PrintDefaults()
+		fmt.Println("\nПример запуска:")
+		fmt.Println("  go run ./app         - Запуск приложения в интерактивном режиме")
+		fmt.Println("  go run ./app --info  - Вывод пояснений по архитектуре проекта")
+		fmt.Println("  go run ./app --help  - Показать это справочное окно")
+	}
+	flag.Parse()
+	if *helpFlag {
+		printProjectInfo()
+		os.Exit(0)
+	}
+
 	cfg := config.LoadConfig()
 	store := notes.NewStore("notes.json")
 	service := notes.NewService(store)
 	ui := notes.NewCLI(service)
+
 	fmt.Println("Приложение запущено.")
 	fmt.Printf("Приветствуем вас, %s!\n", cfg.DefaultUser)
 	ui.Run()
+}
+
+func printProjectInfo() {
+	fmt.Println("Информация о проекте")
+	fmt.Println("\nОкружение:")
+	fmt.Println("   + для работы требуется установленный Go (версии 1.21+).")
+	fmt.Println("   + данные сохраняются локально в корень проекта: notes.json.")
+	fmt.Println("\nСлои архитектуры:")
+	fmt.Println("   + models (types.go):    структура данных заметки.")
+	fmt.Println("   + storage.go (Store):   сохранение JSON, потокобезопасность (sync.Mutex).")
+	fmt.Println("   + service.go (Service): бизнес-логика и валидация.")
+	fmt.Println("   + interface.go (CLI):   интерактивное консольное меню.")
+	fmt.Println("\nКоманды запуска из корня проекта:")
+	fmt.Println("   + go run ./app          - запуск приложения.")
 }
