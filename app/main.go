59b42230d416cc84a4117578e9384665a034d61f
Polina <asugjjgjcvgffj@gmail.com>
2026-05-25 00:44:46 +0700

    Добавила флаг для выбора файла хранения заметок

--- app/main.go
+++ app/main.go
@@ -10,14 +10,16 @@ import (
 
 func main() {
 	helpFlag := flag.Bool("info", false, "Показать подробное пояснение к запуску проекта и архитектуре")
+	StorageFile := flag.String("file", "notes.json", "Файл для хранения заметок")
 	flag.Usage = func() {
 		fmt.Println("<МЕНЕДЖЕР ЗАМЕТОК>\nинструкция по использованию")
 		fmt.Println("\nДоступные флаги:")
 		flag.PrintDefaults()
 		fmt.Println("\nПример запуска:")
-		fmt.Println("  go run ./app         - Запуск приложения в интерактивном режиме")
-		fmt.Println("  go run ./app --info  - Вывод пояснений по архитектуре проекта")
-		fmt.Println("  go run ./app --help  - Показать это справочное окно")
+		fmt.Println("  go run ./app                  - Запуск приложения в интерактивном режиме")
+		fmt.Println("  go run ./app --info           - Вывод пояснений по архитектуре проекта")
+		fmt.Println("  go run ./app --help  	        - Показать это справочное окно")
+		fmt.Println("  go run ./app --file=file.json - Запуск с пользовательским файлом для хранения заметок")
 	}
 	flag.Parse()
 	if *helpFlag {
@@ -26,11 +28,12 @@ func main() {
 	}
 
 	cfg := config.LoadConfig()
-	store := notes.NewStore("notes.json")
+	store := notes.NewStore(*StorageFile)
 	service := notes.NewService(store)
 	ui := notes.NewCLI(service)
 
 	fmt.Println("Приложение запущено.")
+	fmt.Printf("Файл для сохранения заметок: %s\n\n", *StorageFile)
 	fmt.Printf("Приветствуем вас, %s!\n", cfg.DefaultUser)
 	ui.Run()
 }
